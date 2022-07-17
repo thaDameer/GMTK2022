336f5c534dfb8ac421b191658e48bd5ceb1fa64d
thaDameer <38245328+thaDameer@users.noreply.github.com>
2022-07-17 17:38:10 +0200

    Hide cube canvas after complete level. fix gameplay canvas

--- Assets/Scripts/CubeCanvas.cs
+++ Assets/Scripts/CubeCanvas.cs
@@ -6,13 +6,17 @@ public class CubeCanvas : MonoBehaviour
     {
         EventBroker.Instance.OnLevelCountdownStart += Hide;
         EventBroker.Instance.OnStartLevel += Show;
+        EventBroker.Instance.OnCompleteLevel += Hide;
     }
 
     private void OnDestroy()
     {
         EventBroker.Instance.OnLevelCountdownStart -= Hide;
         EventBroker.Instance.OnStartLevel -= Show;
+        EventBroker.Instance.OnCompleteLevel -= Hide;
     }
+
+    private void Hide() => Display(false);
     private void Hide(float value) => Display(false);
     private void Show() => Display(true);
     private void Display(bool show)
