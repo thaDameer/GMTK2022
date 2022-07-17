5461a011a40d51625aef9b8658cf5870ba44d35d
Huggarnn <fabian.hugert@gmail.com>
2022-07-17 12:11:29 +0200

    Sticky Jam SFX

--- Assets/Scripts/Jam.cs
+++ Assets/Scripts/Jam.cs
@@ -7,6 +7,10 @@ public class Jam : BaseTile
 {
     //Stickiness determines how many times one needs to press the button to escape
     public float Stickiness;
+    public int ParticleCount = 10; 
+
+    [SerializeField] private AudioClip smudge;
+    [SerializeField] private ParticleSystem particles; 
 
     public override void EnterTile()
     {
@@ -16,6 +20,8 @@ public class Jam : BaseTile
     public override void TileAction()
     {
         Stickiness -= 1;
+        if (smudge != null) AudioSource.PlayClipAtPoint(smudge, transform.position);
+        if (particles != null) particles.Emit(ParticleCount);
         if (Stickiness <= 0) OnTileComplete.Invoke();
     }
 
