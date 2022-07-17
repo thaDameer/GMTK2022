58c888f0e0779dff68f4270e7f3bae3977831701
Huggarnn <fabian.hugert@gmail.com>
2022-07-17 13:52:57 +0200

    Die Thump alternatives

--- Assets/Scripts/RotateTile.cs
+++ Assets/Scripts/RotateTile.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class RotateTile : BaseTile
 {
+    [SerializeField] private AudioClip clip;
+    [SerializeField] private ParticleSystem particles;
+    public int ParticleCount = 10; 
     public override void EnterTile()
     {
         
@@ -11,6 +14,8 @@ public class RotateTile : BaseTile
 
     public override void TileAction()
     {
+        if (clip != null) AudioSource.PlayClipAtPoint(clip, transform.position);
+        if (particles != null) particles.Emit(ParticleCount); 
         OnTileComplete.Invoke(); 
     }
 
